f0353a7a7dedacb15dd84882f145840199a52e1b
Nicholas Devenish <ndevenish@gmail.com>
2016-11-17 23:57:30 +0000

    Add the structure of the old package, and start postprocessing

---
+++ edm/__init__.py
@@ -0,0 +1,5 @@
+
+from .reader import Reader
+from .typereader import reads_type
+
+from .types import Material
---
+++ edm/types.py
@@ -0,0 +1,10 @@
+
+
+from collections import namedtuple
+
+VertexFormat = namedtuple("VertexFormat", ["position", "normal", "texture"])
+
+class Material(object):
+  def __init__(self, fileData):
+    self._fileData = fileData
+    self.vertexformat = fileData["VERTEX_FORMAT"]
