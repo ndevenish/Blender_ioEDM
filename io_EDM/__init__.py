039b8265a5c9cc84317fd478c3f867ecb1f16166
Nicholas Devenish <ndevenish@gmail.com>
2016-11-26 20:06:43 +0000

    Allow importing without blender

--- io_EDM/__init__.py
+++ io_EDM/__init__.py
@@ -9,34 +9,29 @@ bl_info = {
   'category': 'Import-Export',
 }
 
-import bpy
+try:
+  import bpy
 
-# This handles reloads
-# if "bpy" in locals():
-#     import importlib
-#     if "stl_utils" in locals():
-#         importlib.reload(stl_utils)
-#     if "blender_utils" in locals():
-#         importlib.reload(blender_utils)
+  def register():
+    from .io_operators import register as importer_register
+    from .rna import register as rna_register
+    from .panels import register as panels_register
+    rna_register()
+    panels_register()
+    importer_register()
+    bpy.utils.register_module(__name__)
+    
+  def unregister():
+    from .io_operators import unregister as importer_unregister
+    from .rna import unregister as rna_unregister
+    from .panels import unregister as panels_unregister
+    importer_unregister()
+    panels_unregister()
+    rna_unregister()
+    bpy.utils.unregister_module(__name__)
 
-
-def register():
-  from .io_operators import register as importer_register
-  from .rna import register as rna_register
-  from .panels import register as panels_register
-  rna_register()
-  panels_register()
-  importer_register()
-  bpy.utils.register_module(__name__)
-  
-def unregister():
-  from .io_operators import unregister as importer_unregister
-  from .rna import unregister as rna_unregister
-  from .panels import unregister as panels_unregister
-  importer_unregister()
-  panels_unregister()
-  rna_unregister()
-  bpy.utils.unregister_module(__name__)
-
-if __name__ == "__main__":
-  register()
+  if __name__ == "__main__":
+    register()
+except ImportError:
+  # Allow for now, as we might just want to import the sub-package
+  pass
