1d3fc379636cd71f53d313510ea7d18ac2a26d27
Nicholas Devenish <ndevenish@gmail.com>
2016-12-06 19:53:44 +0000

    Add a command to the write script to allow filename

--- utils/write.py
+++ utils/write.py
@@ -14,8 +14,14 @@ def _main(args):
     import io_EDM
     io_EDM.register()
 
+  try:
+    myArgumentIndex = next(i for i, v in enumerate(sys.argv) if v == "--")
+    args = args[myArgumentIndex+1:]
+  except StopIteration:
+    pass
+  filepath = args[0] if args else "test.edm"
   # Call the import operator
-  bpy.ops.export_mesh.edm(filepath="test.edm")
+  bpy.ops.export_mesh.edm(filepath=filepath)
 
 if __name__ == "__main__":
   if _main(sys.argv) == -1:
