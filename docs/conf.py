f904a8fbc7d0e1080e8b42854e363f37328bb456
Nicholas Devenish <ndevenish@gmail.com>
2017-01-06 19:16:54 +0000

    Convert EDM spec to reStructuredText

--- docs/conf.py
+++ docs/conf.py
@@ -156,11 +156,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
